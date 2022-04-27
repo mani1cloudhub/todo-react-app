16a6592d0b574952b8c8fab30e9d29fd898eee72
mani1cloudhub <102656106+mani1cloudhub@users.noreply.github.com>
2022-04-27 14:12:34 +0530

    Update Constants.js

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 /* For Best Practices https://facebook.github.io/create-react-app/docs/adding-custom-environment-variables*/
-export const API_URL = 'https://todo-env.eba-txkxsxsc.ap-south-1.elasticbeanstalk.com'
+export const API_URL = 'http://todo-env.eba-txkxsxsc.ap-south-1.elasticbeanstalk.com'
 //'http://localhost:5000'
 //export const API_URL = 'http://rest-api-qa.kvajmpmiiu.us-east-1.elasticbeanstalk.com'
 export const JPA_API_URL = `${API_URL}/jpa`
